9af3ceca137474d635a634bba4fc2dfc468964aa
john-cheval <johncheval1@gmail.com>
2025-10-16 16:56:15 +0400

    added the product nested category

--- components/shared/Inner/Innertop/index.tsx
+++ components/shared/Inner/Innertop/index.tsx
@@ -3,14 +3,15 @@ import {
   ProductFiltersProSortSection,
   ProductListCount,
 } from '@graphcommerce/magento-product'
-import { Box, Link, SxProps, Theme, Typography } from '@mui/material'
+import { Box, Link, type SxProps, type Theme, Typography } from '@mui/material'
 
 import {
   iconArrowDropDown,
   iconArrowDropDownUp,
 } from '../../../../plugins/icons'
-import { ProductListLayoutProps } from '../../../ProductListLayout'
+import type { ProductListLayoutProps } from '../../../ProductListLayout'
 import { useRouter } from 'next/router'
+import { Image } from '@graphcommerce/image'
 
 type InnerTopBaseProps = {
   count?: number | null
@@ -52,9 +53,15 @@ export function InnerTop(props: InnerTopProps) {
     category,
   } = props
 
-  const isSubCategory = category?.children?.length > 0
+
+
+  const isSubCategory = category?.children && category?.children?.length > 0
 
   const currentPath = router.asPath?.split('/')?.filter(Boolean)[1]
+
+  const isSamePath = category?.children?.filter((item) => `/${item?.url_path}` === router?.asPath)
+
+  const isLast = isSamePath?.length > 0
   return (
     <Box component='section' className='container-wrapper'>
       <Box
@@ -80,41 +87,97 @@ export function InnerTop(props: InnerTopProps) {
       >
         {/* SubCategory Section */}
         {isSubCategory && (
-          <Box component='div' sx={{
-            display: 'flex',
-            columnGap: '16px',
-          }}>
-            {category?.children?.map((item: any, index) => {
-              const splittedUrl = item?.url_path?.split('/').filter(Boolean)?.[1]
-              const activeButton = currentPath === splittedUrl
-              return (
-                <Link href={item?.url_path} key={`index-${index + 1}`}
-                  sx={{
-                    background: theme => activeButton ? theme.palette.custom.tltMain : theme.palette.custom.tltSecondary,
-                    color: theme => theme.palette.custom.tltContrastText,
-                    borderRadius: '3px',
-                    textAlign: 'center',
-                    fontsize: { xs: '16px', md: '20px', lg: '25px' },
-                    fontWeight: 700,
-                    lineHeight: '120%',
-                    padding: '25px 30px',
-                    minWidth: { xs: '150px', md: '215px' },
-                    textDecoration: 'none',
-                    transition: 'all 0.4s ease-in-out',
-                    borderColor: theme => activeButton ? theme.palette.custom.tltMain : theme.palette.custom.tltSecondary,
-                    borderWidth: '1px',
-                    borderStyle: 'solid',
+          <>
+            <Box component='div' sx={{
+              display: 'flex',
+              columnGap: '16px',
+            }}>
 
-                    '&:hover': {
-                      background: 'transparent',
+              {/* Button RType Level 3 */}
+              {category?.children?.filter((items) => items?.level === 3)?.map((item, index) => {
+                const splittedUrl = item?.url_path?.split('/').filter(Boolean)?.[1]
+                const activeButton = currentPath === splittedUrl
+                return (
+                  <Link href={item?.url_path} key={`index-${index + 1}`}
+                    sx={{
+                      background: theme => activeButton ? theme.palette.custom.tltMain : theme.palette.custom.tltSecondary,
+                      color: theme => theme.palette.custom.tltContrastText,
+                      borderRadius: '3px',
+                      textAlign: 'center',
+                      fontsize: { xs: '16px', md: '20px', lg: '25px' },
+                      fontWeight: 700,
+                      lineHeight: '120%',
+                      padding: '25px 30px',
+                      minWidth: { xs: '150px', md: '215px' },
+                      textDecoration: 'none',
+                      transition: 'all 0.4s ease-in-out',
                       borderColor: theme => activeButton ? theme.palette.custom.tltMain : theme.palette.custom.tltSecondary,
-                      color: theme => activeButton ? theme.palette.custom.tltMain : theme.palette.custom.tltSecondary,
-                    },
+                      borderWidth: '1px',
+                      borderStyle: 'solid',
+                      '&:hover': {
+                        background: 'transparent',
+                        borderColor: theme => activeButton ? theme.palette.custom.tltMain : theme.palette.custom.tltSecondary,
+                        color: theme => activeButton ? theme.palette.custom.tltMain : theme.palette.custom.tltSecondary,
+                      },
 
-                  }}>{item?.name}</Link>
-              )
-            })}
-          </Box>
+                    }}>
+                    {item?.name}
+                  </Link>
+                )
+              })}
+            </Box>
+
+            {!isLast && (
+              <Box
+                component='div'
+                sx={{
+                  display: 'grid',
+                  gridTemplateColumns: { xs: 'repeat(2,1r)', md: 'repeat(3,1fr)', lg: 'repeat(5,1fr)', xl: 'repeat(6,1fr)' },
+                  columnGap: { xs: '10px', md: '18px' },
+                  rowGap: { xs: '10px', md: '20px', lg: '30px' },
+                }}
+              >
+                {category?.children?.filter((items) => items?.level === 4)?.map((item, index) => {
+                  const splittedUrl = item?.url_path?.split('/').filter(Boolean)?.[2]
+                  // const activeButton = currentPath === splittedUrl
+
+                  // console.log(splittedUrl, '==> splittedUrl')
+                  return (
+                    <Link
+                      key={`index-${index + 1}`}
+                      href={`/${item?.url_path}`}
+                      sx={{
+                        position: 'relative',
+                        height: 'fit-content',
+                        transition: 'all 0.4s ease-in-out',
+                        '& p': {
+                          bottom: 0,
+                        },
+                        '&:hover': {
+                          transform: 'translateY(-10px)',
+                        },
+                      }}
+                    >
+                      <Image
+                        src={item?.image}
+                        alt={item?.name}
+                        width={230}
+                        height={315}
+                        sx={{
+                          width: '100%',
+                          height: 'auto',
+                          objectFit: 'cover',
+                          // maxWidth: '130px',
+                        }}
+                      />
+                      <Typography className='unlock-title' >{item?.name}</Typography>
+                    </Link>
+                  )
+                })}
+              </Box>
+            )}
+
+          </>
         )}
 
 
@@ -146,96 +209,101 @@ export function InnerTop(props: InnerTopProps) {
                 {title}
               </Typography>
 
-              <ProductListCount
-                total_count={count}
-                sx={{
-                  gridArea: 'count',
-                  width: '100%',
-                  my: 0,
-                  height: '1em-',
-                  '& p': {
-                    textAlign: { xs: 'right', md: 'left' },
-                    color: '#747474',
-                    fontSize: { xs: '16px', md: '18px' },
-                    lineHeight: '120%',
-                    fontWeight: 400,
-                  },
-
-                }}
-              />
-            </Box>
-
-
-            <Box sx={{ display: { xs: 'none', md: 'block' } }}>
-              {params && products?.items && filterTypes && (
-                <ProductFiltersProSortSection
-                  menu={menu}
-                  sort_fields={products?.sort_fields}
-                  total_count={products?.total_count}
-                  category={category}
-                  openAccordionIcon={iconArrowDropDown}
-                  closeAccordionIcon={iconArrowDropDownUp}
+              {count && (
+                <ProductListCount
+                  total_count={count}
                   sx={{
-                    borderBottom: 'none !important',
-                    '& .MuiAccordionSummary-content .MuiTypography-body1': {
-                      color: (theme: any) => theme.palette.custom.dark,
-                      fontSize: { xs: '15px', md: '18px' },
-                      marginBottom: '0 !important',
-                      position: 'relative',
+                    gridArea: 'count',
+                    width: '100%',
+                    my: 0,
+                    height: '1em-',
+                    '& p': {
+                      textAlign: { xs: 'right', md: 'left' },
+                      color: '#747474',
+                      fontSize: { xs: '16px', md: '18px' },
+                      lineHeight: '120%',
+                      fontWeight: 400,
                     },
-                    '& .MuiAccordionDetails-root > div': {
-                      position: 'absolute',
-                      backgroundColor: '#fff',
-                      width: '100%',
-                      borderRadius: '4px',
-                      zIndex: 1000,
-                      minWidth: '200px',
-                      border: (theme) => `1px solid ${theme.palette.custom.tltSecondary}`,
-                      '& .ActionCard-root': {
-                        borderRadius: '0',
-                      },
-                      '& .ActionCardLayout-root ': {
-                        border: (theme) => theme.palette.custom.border,
-                        borderRadius: '3px',
-                        '& .MuiButtonBase-root': {
-                          '&:not(:last-child)': {
-                            borderBottom: (theme) => `1px solid ${theme.palette.custom.tltSecondary}`,
-                          },
-                          '& .ActionCard-title': {
-                            fontSize: { xs: '15px', md: '16px' },
-                            fontWeight: 400,
-                          },
-                          '& .ActionCard-end': {
-                            display: 'none',
-                          },
-                        },
-                      },
-                      '& .ActionCard-root.selected': {
-                        backgroundColor: (theme) => theme.palette.custom.border,
-                      },
-                    },
-                    '& .ActionCardLayout-root ': {
-                      backgroundColor: 'white',
-                    },
-                    '& .ActionCard-root.selected': {
-                      backgroundColor: (theme) => theme.palette.custom.border,
-                    },
-                    '& .MuiAccordionSummary-expandIconWrapper': {
-                      position: 'relative',
-                      top: '8px',
-                      marginLeft: '5px',
-                    },
+
                   }}
-
                 />
-
               )}
             </Box>
+
+
+            {/* {count && count > 0 && (
+                <Box sx={{ display: { xs: 'none', md: 'block' } }}>
+                  {params && products?.items && filterTypes && (
+                    <ProductFiltersProSortSection
+                      menu={menu}
+                      sort_fields={products?.sort_fields}
+                      total_count={products?.total_count}
+                      category={category}
+                      openAccordionIcon={iconArrowDropDown}
+                      closeAccordionIcon={iconArrowDropDownUp}
+                      sx={{
+                        borderBottom: 'none !important',
+                        '& .MuiAccordionSummary-content .MuiTypography-body1': {
+                          color: (theme: any) => theme.palette.custom.dark,
+                          fontSize: { xs: '15px', md: '18px' },
+                          marginBottom: '0 !important',
+                          position: 'relative',
+                        },
+                        '& .MuiAccordionDetails-root > div': {
+                          position: 'absolute',
+                          backgroundColor: '#fff',
+                          width: '100%',
+                          borderRadius: '4px',
+                          zIndex: 1000,
+                          minWidth: '200px',
+                          border: (theme) => `1px solid ${theme.palette.custom.tltSecondary}`,
+                          '& .ActionCard-root': {
+                            borderRadius: '0',
+                          },
+                          '& .ActionCardLayout-root ': {
+                            border: (theme) => theme.palette.custom.border,
+                            borderRadius: '3px',
+                            '& .MuiButtonBase-root': {
+                              '&:not(:last-child)': {
+                                borderBottom: (theme) => `1px solid ${theme.palette.custom.tltSecondary}`,
+                              },
+                              '& .ActionCard-title': {
+                                fontSize: { xs: '15px', md: '16px' },
+                                fontWeight: 400,
+                              },
+                              '& .ActionCard-end': {
+                                display: 'none',
+                              },
+                            },
+                          },
+                          '& .ActionCard-root.selected': {
+                            backgroundColor: (theme) => theme.palette.custom.border,
+                          },
+                        },
+                        '& .ActionCardLayout-root ': {
+                          backgroundColor: 'white',
+                        },
+                        '& .ActionCard-root.selected': {
+                          backgroundColor: (theme) => theme.palette.custom.border,
+                        },
+                        '& .MuiAccordionSummary-expandIconWrapper': {
+                          position: 'relative',
+                          top: '8px',
+                          marginLeft: '5px',
+                        },
+                      }}
+
+                    />
+
+                  )}
+                </Box>
+              )} */}
+
 
           </Box>
         )}
 
-        {responsiveTitle && (
+        {/* {responsiveTitle && (
           <Typography
             component='h2'
             variant='h2'
@@ -251,7 +319,7 @@ export function InnerTop(props: InnerTopProps) {
           >
             {responsiveTitle}
           </Typography>
-        )}
+        )} */}
       </Box>
     </Box>
   )
