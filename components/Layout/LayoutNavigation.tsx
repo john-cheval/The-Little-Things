42a72531efd1c283e2c5e903e4c4638387b9703a
john-cheval <johncheval1@gmail.com>
2025-10-28 23:07:32 +0400

    terms condition page responsive has been done

--- components/Layout/LayoutNavigation.tsx
+++ components/Layout/LayoutNavigation.tsx
@@ -107,21 +107,16 @@ export function LayoutNavigation(props: LayoutNavigationProps) {
       name: 'FAQs',
       path: '/faq',
     },
-  ]
-
-
-  const noMenus = [
     {
-      name: 'FAQs',
-      path: '/faq',
+      name: 'Terms & Conditions',
+      path: '/terms-conditions',
     },
   ]
 
   const isSmallScreen = useMediaQuery('(max-width:768px)')
 
   const isStaticPage = staticPageLinks?.filter((item) => item?.path === router?.pathname)
-  const noMenuPages = noMenus?.filter((item) => item?.path === router?.pathname)
-  const isPaddingTop = (noMenuPages || isStaticPage) && isSmallScreen;
+  const isPaddingTop = isStaticPage && isSmallScreen;
 
 
   const handleBack = () => {
@@ -168,6 +163,7 @@ export function LayoutNavigation(props: LayoutNavigationProps) {
               // borderBottom: theme => `1px solid ${theme.palette.custom.tltBorder1}`,
               borderBottom: { xs: 0, lg: theme => `1px solid ${theme.palette.custom.tltBorder1}` },
               transition: 'all 0.4s ease-in-out',
+              paddingBottom: { xs: '10px', md: 0 },
             }}>
               {(isStaticPage && isStaticPage?.length > 0 && isSmallScreen) ? (
                 <Box
@@ -178,7 +174,7 @@ export function LayoutNavigation(props: LayoutNavigationProps) {
                     display: { xs: 'flex', md: 'none' },
                     alignItems: 'center',
                     gap: '5px',
-                    paddingBottom: '10px',
+                    paddingBottom: '5px',
                   }}>
                   <IoIosArrowBack size={20} />
                   <Typography
@@ -257,191 +253,185 @@ export function LayoutNavigation(props: LayoutNavigationProps) {
                 />
               </DesktopNavActions>
 
-              {(noMenuPages && noMenuPages?.length > 0 && isSmallScreen) ? (
-                null
-              ) : (
-                <DesktopNavActions
+              <DesktopNavActions
+                sx={{
+                  '& .MuiformControl': {
+                    border: 'none',
+                  },
+                }}
+              >
+
+                <CurrecySelctor />
+                <LangauageSelctor />
+                <WishlistFab
                   sx={{
-                    '& .MuiformControl': {
-                      border: 'none',
+                    width: { xs: '36px' },
+                    height: { xs: '30px', md: '36px' },
+                    color: (theme) => theme.palette.custom.main,
+                    '&  svg': {
+                      fontSize: { xs: '20px' },
+                      strokeWidth: '1.5',
+                    },
+                    ' &:focus ': {
+                      backgroundColor: 'transparent',
+                    },
+                    '&:hover': {
+                      backgroundColor: 'transparent',
                     },
                   }}
-                >
+                  icon={
+                    <IconSvg
+                      src={iconHeart}
+                      size='medium'
+                      sx={{ stroke: (theme) => theme.palette.custom.main }}
+                    />
+                  }
+                />
 
-                  <CurrecySelctor />
-                  <LangauageSelctor />
-                  <WishlistFab
-                    sx={{
-                      width: { xs: '36px' },
-                      height: { xs: '30px', md: '36px' },
-                      color: (theme) => theme.palette.custom.main,
-                      '&  svg': {
-                        fontSize: { xs: '20px' },
-                        strokeWidth: '1.5',
-                      },
-                      ' &:focus ': {
-                        backgroundColor: 'transparent',
-                      },
-                      '&:hover': {
-                        backgroundColor: 'transparent',
-                      },
-                    }}
-                    icon={
-                      <IconSvg
-                        src={iconHeart}
-                        size='medium'
-                        sx={{ stroke: (theme) => theme.palette.custom.main }}
-                      />
-                    }
-                  />
+                <CartFab
+                  sx={{
+                    display: { xs: 'none', lg: 'inline-flex' },
+                    color: (theme) => theme.palette.custom.textDarkAlter2,
+                    width: { md: 'fit-content' },
 
-                  <CartFab
-                    sx={{
-                      display: { xs: 'none', lg: 'inline-flex' },
-                      color: (theme) => theme.palette.custom.textDarkAlter2,
-                      width: { md: 'fit-content' },
+                    transition: 'all 0.4s ease-in-out',
+                    '&:focus': {
+                      backgroundColor: 'transparent',
+                    },
+                    '&:hover': {
+                      backgroundColor: 'transparent',
+                    },
+                    '& .MuiBadge-root': {
+                      left: '5px',
+                      top: '3px',
 
-                      transition: 'all 0.4s ease-in-out',
-                      '&:focus': {
-                        backgroundColor: 'transparent',
+                      '& .MuiBadge-badge': {
+                        top: '5px',
+                        right: '11px',
                       },
-                      '&:hover': {
-                        backgroundColor: 'transparent',
-                      },
-                      '& .MuiBadge-root': {
-                        left: '5px',
-                        top: '3px',
+                    },
+                    '& svg': {
+                      fontSize: { md: '30px' },
+                      stroke: 'unset !important',
+                    },
+                    '& .MuiButtonBase-root': {
+                      height: 'unset !important',
+                      width: 'unset !important',
+                      boxShadow: 'none',
+                      backgroundColor: 'transparent !important',
+                    },
+                    '& .mui-style-1jnnhmg-MuiButtonBase-root-MuiFab-root': {
+                      minHeight: 'unset',
+                      boxShadow: 'none',
+                      backgroundcolor: 'transparent',
+                      transition: 'unset',
+                    },
+                  }}
+                />
+                <CustomerFab
+                  sx={{
+                    width: { md: '36px' },
+                    height: { md: '35px' },
+                    display: { xs: 'none', lg: 'inline-flex' },
+                    color: (theme) => theme.palette.custom.textDarkAlter2,
 
-                        '& .MuiBadge-badge': {
-                          top: '5px',
-                          right: '11px',
-                        },
+                    '&  svg': {
+                      // width: '1em',
+                      fontSize: { md: '25px' },
+                      stroke: 'unset !important',
+                    },
+                    '&:focus': {
+                      backgroundColor: 'transparent',
+                    },
+                    '&:hover': {
+                      backgroundColor: 'transparent',
+                    },
+                    '& .MuiBadge-root': {
+                      left: '0px',
+                      top: '3px',
+                      '& .MuiBadge-dot': {
+                        display: 'none',
                       },
-                      '& svg': {
-                        fontSize: { md: '30px' },
-                        stroke: 'unset !important',
-                      },
-                      '& .MuiButtonBase-root': {
-                        height: 'unset !important',
-                        width: 'unset !important',
-                        boxShadow: 'none',
-                        backgroundColor: 'transparent !important',
-                      },
-                      '& .mui-style-1jnnhmg-MuiButtonBase-root-MuiFab-root': {
-                        minHeight: 'unset',
-                        boxShadow: 'none',
-                        backgroundcolor: 'transparent',
-                        transition: 'unset',
-                      },
-                    }}
-                  />
-                  <CustomerFab
-                    sx={{
-                      width: { md: '36px' },
-                      height: { md: '35px' },
-                      display: { xs: 'none', lg: 'inline-flex' },
-                      color: (theme) => theme.palette.custom.textDarkAlter2,
+                    },
+                  }}
+                  guestHref='/account/signin'
+                  authHref='/account'
+                />
 
-                      '&  svg': {
-                        // width: '1em',
-                        fontSize: { md: '25px' },
-                        stroke: 'unset !important',
-                      },
-                      '&:focus': {
-                        backgroundColor: 'transparent',
-                      },
-                      '&:hover': {
-                        backgroundColor: 'transparent',
-                      },
-                      '& .MuiBadge-root': {
-                        left: '0px',
-                        top: '3px',
-                        '& .MuiBadge-dot': {
-                          display: 'none',
-                        },
-                      },
-                    }}
-                    guestHref='/account/signin'
-                    authHref='/account'
-                  />
+                <Link
+                  href='/popmart'
+                  sx={{
+                    display: { xs: 'none', lg: 'block' },
+                    // overflow: 'hidden',
+                    transition: 'transform 0.4s ease-in-out',
+                    '&:hover': {
+                      transform: 'scale(1.05)',
+                    },
+                    '& img': {
+                      borderRadius: '3px',
+                      height: 'auto',
+                      width: { xs: '120px' },
+                      objectFit: 'cover',
+                    },
 
-                  <Link
-                    href='/popmart'
-                    sx={{
-                      display: { xs: 'none', lg: 'block' },
-                      // overflow: 'hidden',
-                      transition: 'transform 0.4s ease-in-out',
-                      '&:hover': {
-                        transform: 'scale(1.05)',
-                      },
-                      '& img': {
-                        borderRadius: '3px',
-                        height: 'auto',
-                        width: { xs: '120px' },
-                        objectFit: 'cover',
-                      },
-
-                    }}>
-                    <Image src={popmartImage} alt='popmartImage' />
-                  </Link>
-                </DesktopNavActions>
-              )}
+                  }}>
+                  <Image src={popmartImage} alt='popmartImage' />
+                </Link>
+              </DesktopNavActions>
 
             </Box>
 
             {/* Search field for small Screens */}
 
-            {(noMenuPages && noMenuPages?.length > 0 && isSmallScreen) ? null : (
-              <DesktopNavActions
-                sx={{
-                  width: '100%',
-                  display: { xs: 'flex', md: 'none' },
+            <DesktopNavActions
+              sx={{
+                width: '100%',
+                display: { xs: 'flex', md: 'none' },
 
-                }}>
-                <SearchField
-                  visible
-                  isNav
-                  formControl={{
-                    sx: {
-                      width: '100%',
+              }}>
+              <SearchField
+                visible
+                isNav
+                formControl={{
+                  sx: {
+                    width: '100%',
+                    color: (theme: any) => theme.palette.custom.textDarkAlter2,
+                    '& .MuiOutlinedInput-root': {
                       color: (theme: any) => theme.palette.custom.textDarkAlter2,
-                      '& .MuiOutlinedInput-root': {
-                        color: (theme: any) => theme.palette.custom.textDarkAlter2,
-                        fontSize: '14px',
-                        borderRadius: '3px',
-                        background: 'transparent',
-                        paddingRight: '0px',
-                        width: '100%',
-                        '& input': {
-                          padding: '15px 12px',
-                        },
-                        '& .MuiOutlinedInput-input, & .MuiOutlinedInput-input::placeholder': {
-                          fontSize: '14px',
-                          fontStyle: 'normal',
-                          fontWeight: 400,
-                          lineHeight: '158%',
-                          color: (theme: any) => theme.palette.custom.textDarkAlter2,
-                          opacity: 1,
-                        },
-                        '& .MuiOutlinedInput-notchedOutline': {
-                          borderColor: (theme: any) => theme.palette.custom.tltBorder4,
-                        },
-                        '&:hover .MuiOutlinedInput-notchedOutline': {
-                          borderColor: (theme: any) => theme.palette.custom.tltBorder4,
-                        },
-
-                        '&.Mui-focused .MuiOutlinedInput-notchedOutline': {
-                          borderColor: (theme: any) => theme.palette.custom.tltBorder4,
-                        },
-
+                      fontSize: '14px',
+                      borderRadius: '3px',
+                      background: 'transparent',
+                      paddingRight: '0px',
+                      width: '100%',
+                      '& input': {
+                        padding: '15px 12px',
                       },
-                    },
-                  }}
-                  searchField={{ productListRenderer }}
+                      '& .MuiOutlinedInput-input, & .MuiOutlinedInput-input::placeholder': {
+                        fontSize: '14px',
+                        fontStyle: 'normal',
+                        fontWeight: 400,
+                        lineHeight: '158%',
+                        color: (theme: any) => theme.palette.custom.textDarkAlter2,
+                        opacity: 1,
+                      },
+                      '& .MuiOutlinedInput-notchedOutline': {
+                        borderColor: (theme: any) => theme.palette.custom.tltBorder4,
+                      },
+                      '&:hover .MuiOutlinedInput-notchedOutline': {
+                        borderColor: (theme: any) => theme.palette.custom.tltBorder4,
+                      },
 
-                />
-              </DesktopNavActions>
-            )}
+                      '&.Mui-focused .MuiOutlinedInput-notchedOutline': {
+                        borderColor: (theme: any) => theme.palette.custom.tltBorder4,
+                      },
+
+                    },
+                  },
+                }}
+                searchField={{ productListRenderer }}
+
+              />
+            </DesktopNavActions>
 
 
             {/* Small screen International shipping Banner */}
